32ea8a29c9aca0488b3dfceaa3ae415881468ab3
lucian <lucian.dizzy@gmail.com>
2017-03-16 11:24:23 +0100

    algo es algo

---
+++ prac1.py
@@ -0,0 +1,46 @@
+import sys
+
+def readFile(f):
+
+	f = open(f, "r")
+	itemList = []
+	for line in f.readlines():
+		parsed_line = line.split();
+		itemList.append((parsed_line[0],parsed_line[1]))
+
+	return itemList
+
+
+def trol_sort_it(li):	
+	cur_max_h = 0
+	cur_max_l = []
+	max_l = []
+	
+	for tup in li:
+		#each element
+		cur_max_l = find_max(li[li.index(tup):])
+		if len(cur_max_l) > len(max_l):
+			max_l = cur_max_l
+
+	return max_l
+
+
+def find_max(li):
+	max_l = []
+	last = 0
+	for line in li:
+		if line[1] > last:
+			max_l.append(line)
+			last = line[1]
+	return max_l
+
+
+def trol_sort_rec(li):
+	#to do
+	pass
+
+
+if __name__ == '__main__':
+
+	p = readFile('cargament.txt')
+	print trol_sort_it(p)
